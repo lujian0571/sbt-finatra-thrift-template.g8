ee5dde85099d6e8d95549cfec958767a8c0918a2
卢键 <254461627@qq.com>
2017-07-11 13:19:52 +0800

    add import

--- src/main/g8/server/src/main/scala/$organization__packaged$/$name__norm$/$name__Camel$Server.scala
+++ src/main/g8/server/src/main/scala/$organization__packaged$/$name__norm$/$name__Camel$Server.scala
@@ -3,6 +3,7 @@ package $organization;format="normalize"$.$name;format="normalize"$
 import com.twitter.finatra.thrift.ThriftServer
 import com.twitter.finatra.thrift.routing.ThriftRouter
 import com.twitter.finatra.thrift.filters._
+import com.twitter.finatra.thrift.modules.ClientIdWhitelistModule
 import $organization;format="normalize"$.$name;format="normalize"$.controllers.$name;format="Camel"$Controller
 import $organization;format="normalize"$.$name;format="normalize"$.filters.ExceptionTranslationFilter
 import $organization;format="normalize"$.$name;format="normalize"$.modules.ServicesModule
